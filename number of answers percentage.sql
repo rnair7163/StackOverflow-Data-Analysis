1e9e7945fa847e227edebbe26cfadf126907521e
Shouvik Sunil Sharma <shouvik19@gmail.com>
2019-11-17 19:03:36 -0600

    Update number of answers percentage.sql

--- number of answers percentage.sql
+++ number of answers percentage.sql
@@ -14,3 +14,27 @@ and A.owner_user_id in (469335,
 7148391,
 654031)
 group by A.owner_user_id;
+
+file = LOAD 'post_answers.csv' Using PigStorage(',') AS (ID:int,title:chararray,body:chararray,
+accepted_answer_id:chararray,
+answer_count:chararray,
+comment_count:int,
+community_owned_date:int,
+creation_date:chararray
+favorite_count:chararray
+last_activity_date:chararray
+last_edit_date:chararray,
+last_editor_display_name:chararray
+last_editor_user_id:int,
+owner_display_name:chararray,
+owner_user_id:int,
+parent_id:int,
+post_type_id:int,
+score:int,
+tags:chararray,
+view_count:chararray
+);
+
+grpd  = group file by owner_user_id;
+cnt   = foreach grpd generate group, COUNT(distinct id);
+		
